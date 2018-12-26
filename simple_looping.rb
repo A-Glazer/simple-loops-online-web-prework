823251a5407241056e5ecb18bd20d4ee5e2164d8
A Glazer <avigayilglazer@gmail.com>
2018-12-26 13:21:08 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -5,6 +5,7 @@ def loop_iterator(number_of_times)
     puts "Welcome to Flatiron School's Web Development Course!"
   end
 end
+end
 
 loop_iterator(6)
   # Code your solution here using the "loop" keyword to puts out the below phrase
