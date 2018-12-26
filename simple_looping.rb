ace3c10e47c896e10f976add603213e8c6a373ba
A Glazer <avigayilglazer@gmail.com>
2018-12-26 13:28:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,11 +1,11 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
-def loop_iterator(7_)
+def loop_iterator(number_of_times)
   counter = 0
   loop do
     counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    if counter >= 7
+    if counter >= number_of_times
       break
     end
   end
