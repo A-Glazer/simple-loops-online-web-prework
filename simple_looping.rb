4c8beda9fdddb9836225dbbfd8a1608f2b30ce1a
A Glazer <avigayilglazer@gmail.com>
2018-12-26 13:21:58 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,6 +1,7 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
+  number_of_times = 6
   (number_of_times).times.do
     puts "Welcome to Flatiron School's Web Development Course!"
   end
